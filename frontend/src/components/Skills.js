4e5fc32735de8566b81089b51aae3b95227686a6
Jehu Silva <jehusilva@MacBook-Pro.local>
2023-11-11 21:42:38 -0600

    Commiting design and developing of skills

---
+++ frontend/src/components/Skills.js
@@ -0,0 +1,63 @@
+import awsSVG from "../assets/img/aws-icon.svg";
+import pythonSVG from "../assets/img/python-icon.svg";
+import gcpSVG from "../assets/img/gcp-icon.svg";
+import playwrightSVG from "../assets/img/playwright-icon.svg";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import colorSharp from "../assets/img/color-sharp.png"
+
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+  return (
+    <section className="skill" id="skills">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Skills</h2>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+              <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                <div className="item">
+                  <img src={awsSVG} alt="" />
+                  <h5>Amazon Web Services</h5>
+                </div>
+                <div className="item">
+                  <img src={pythonSVG} alt="" />
+                  <h5>python</h5>
+                </div>
+                <div className="item">
+                  <img src={gcpSVG} alt="" />
+                  <h5>Google Cloud Platform</h5>
+                </div>
+                <div className="item">
+                  <img src={playwrightSVG} alt="" />
+                  <h5>Web Scraping</h5>
+                </div>
+              </Carousel>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="" />
+    </section>
+  )
+}
