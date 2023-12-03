7a9aef623736d29435069c238c1bcad894c6f977
Jehu Silva <jehusilva@MacBook-Pro.local>
2023-12-02 21:28:26 -0600

    Infrastructure deployment: Removing dynamo, hosted zone and ACM Certificatie creation. Did it manually istead

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -1,11 +1,9 @@
-
-export DOMAIN=jehusilva.com
-export STACK_NAME=project-personal-portfolio
-
 echo Deploying for $DOMAIN
 
 aws cloudformation deploy \
 --template-file cloudformation-stack.yaml \
 --stack-name $STACK_NAME \
---parameter-overrides DomainName=$DOMAIN SubdomainName=$SUB_DOMAIN \
+--parameter-overrides \
+    DomainName=$DOMAIN \
+    ACMCertificateArn=$CERTIFICATE_ARN \
 --capabilities CAPABILITY_IAM
--- infrastructure/sync_front_end.sh
+++ infrastructure/sync_front_end.sh
@@ -1,4 +1,3 @@
 echo Syncing frontend files
 
-aws s3 sync ../frontend/build/ s3://jehusilva.com/ --exclude "*.tmp" --delete
-# aws s3 cp ../frontend/build/ s3://jehusilva.com/ --recursive
+aws s3 sync ../frontend/build/ s3://$DOMAIN/ --exclude "*.tmp" --delete
